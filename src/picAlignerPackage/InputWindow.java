a803e380d529417af1c38170d3287c244d2093b2
Keegan Kochis <keeganjkochis@gmail.com>
2019-01-31 11:56:15 -0700

    Changes to main JFrame to activate the JFileChooser=

--- src/picAlignerPackage/InputWindow.java
+++ src/picAlignerPackage/InputWindow.java
@@ -4,13 +4,20 @@
 //	Email: 	keeganjkochis@gmail.com
 package picAlignerPackage;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class InputWindow {
 	
-	public Image mainImage;
+	public BufferedImage mainImage;
 	public String inputPath;
 	public String outputPath;
 	
@@ -18,21 +25,39 @@ public class InputWindow {
 	public InputWindow() {
 		//	Create a JFileChooser for selecting mainImage
 		JFileChooser imageChooser = new JFileChooser();
-		FileNameExtensionFilter imageFilter = new FileNameExtensionFilter("JPEG files", "JPEG");
-		imageChooser.setFileFilter(imageFilter);
+		FileNameExtensionFilter jpegFilter = new FileNameExtensionFilter("JPEG files", "JPEG");
+		imageChooser.setFileFilter(jpegFilter);
+		FileNameExtensionFilter jpgFilter = new FileNameExtensionFilter("JPG files", "JPG");
+		imageChooser.setFileFilter(jpgFilter);
 		
 		//	Create a JFrame for getting user input
-		JFrame inputWindow = new JFrame();
-		inputWindow.setTitle("Pic-A-Day Aligner");
-		inputWindow.setSize(500, 500);
+		JFrame mainWindow = new JFrame();
+	    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setTitle("Pic-A-Day Aligner");
+		mainWindow.setSize(500, 500);
 		JButton mainImageButton = new JButton("Choose Baseline Image");
 		mainImageButton.setBounds(50,50,50,100);
-		inputWindow.add(mainImageButton);
-
-		inputWindow.setVisible(true);
+		mainWindow.add(mainImageButton);
 		
-		//	Check if button is pressed
+		mainWindow.setVisible(true);
+		
+		//	Crteate JFrame for choosing base image
+		JFrame imageChooseWindow = new JFrame();
+	    imageChooseWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		imageChooseWindow.add(imageChooser);
+		imageChooseWindow.setSize(500,500);
+		
+		//	Check if button is pressed via ActionListener
+		mainImageButton.addActionListener(new ActionListener() {
+			
+
 			//	Get main image file from user in JFrame using JFileChooser
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Open the file chooser if mainImageButton is pressed and get file
+				imageChooseWindow.setVisible(true);
+			}
+		});
 		
 		//	Get input path for images from user in JFrame
 		
